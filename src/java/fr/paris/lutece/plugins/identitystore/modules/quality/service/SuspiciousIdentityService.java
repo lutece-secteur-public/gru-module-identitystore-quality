bc04655894ee8e432ab5499a67d374f5ffaa6337
Guillaume Comminges <gcomminges@yupiik.com>
2025-02-07 16:21:28 +0100

    LUT-29289 Ajouter un menu Gestion des locks + afficher identités locks dans gestion des doublons

--- src/java/fr/paris/lutece/plugins/identitystore/modules/quality/service/SuspiciousIdentityService.java
+++ src/java/fr/paris/lutece/plugins/identitystore/modules/quality/service/SuspiciousIdentityService.java
@@ -49,6 +49,7 @@ import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.crud.SuspiciousIdenti
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.crud.SuspiciousIdentityExcludeRequest;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.crud.SuspiciousIdentitySearchRequest;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.history.IdentityChangeType;
+import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.lock.SuspiciousIdentityAllLocksResponse;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.dto.lock.SuspiciousIdentityLockRequest;
 import fr.paris.lutece.plugins.identitystore.v3.web.rs.util.Constants;
 import fr.paris.lutece.plugins.identitystore.web.exception.IdentityStoreException;
@@ -74,6 +75,7 @@ public class SuspiciousIdentityService
     private static final String LOCK_SUSPICIOUS_IDENTITY_EVENT_CODE = "LOCK_SUSPICIOUS_IDENTITY";
     private static final String UNLOCK_SUSPICIOUS_IDENTITY_EVENT_CODE = "UNLOCK_SUSPICIOUS_IDENTITY";
     private static final String EXCLUDE_SUSPICIOUS_IDENTITY_EVENT_CODE = "EXCLUDE_SUSPICIOUS_IDENTITY";
+    private static final String ACCESS_SUSPICIOUS_IDENTITY_LOCK_STATUS = "ACCESS_SUSPICIOUS_IDENTITY_LOCK_STATUS";
     private static final String SPECIFIC_ORIGIN = "BO";
 
     // SERVICES
@@ -289,5 +291,50 @@ public class SuspiciousIdentityService
     {
         final List<SuspiciousIdentity> suspiciousIdentities = SuspiciousIdentityHome.selectByCustomerIDs(customerIds);
         return suspiciousIdentities.stream( ).map( SuspiciousIdentityMapper::toDto ).collect( Collectors.toList( ) );
+    }
+
+    public void checkLock( SuspiciousIdentityLockRequest request, String clientCode,
+                           final RequestAuthor author, SuspiciousIdentityLockResponse response   )
+    {
+
+        TransactionManager.beginTransaction( null );
+        try{
+            final boolean locked = SuspiciousIdentityHome.isLock(request.getCustomerId());
+            TransactionManager.commitTransaction( null );
+            response.setCustomerId( request.getCustomerId( ) );
+            response.setLocked( locked );
+            response.setStatus( ResponseStatusFactory.success( ).setMessage( "Identities lock status catch." )
+                    .setMessageKey( Constants.PROPERTY_REST_INFO_SUCCESSFUL_OPERATION ) );
+
+            AccessLogService.getInstance( ).info( AccessLoggerConstants.EVENT_TYPE_READ, ACCESS_SUSPICIOUS_IDENTITY_LOCK_STATUS,
+                    _internalUserService.getApiUser( author, clientCode ), request, SPECIFIC_ORIGIN );
+        }
+        catch( Exception e )
+        {
+            TransactionManager.rollBack( null );
+            response.setStatus(
+                    ResponseStatusFactory.failure( ).setMessage( e.getMessage( ) ).setMessageKey( Constants.PROPERTY_REST_ERROR_DURING_TREATMENT ) );
+        }
+    }
+
+    public void getAllLocks(String clientCode, final RequestAuthor author, final SuspiciousIdentityAllLocksResponse response )
+    {
+        TransactionManager.beginTransaction( null );
+        try{
+            final List<SuspiciousIdentity> suspiciousIdentitysList = SuspiciousIdentityHome.getAllLocks();
+            TransactionManager.commitTransaction( null );
+            response.setSuspiciousIdentityDtoList(suspiciousIdentitysList.stream( ).map( SuspiciousIdentityMapper::toDto ).collect( Collectors.toList( ) ));
+            response.setStatus( ResponseStatusFactory.success( ).setMessage( "Identities lock status catch." )
+                    .setMessageKey( Constants.PROPERTY_REST_INFO_SUCCESSFUL_OPERATION ) );
+
+            AccessLogService.getInstance( ).info( AccessLoggerConstants.EVENT_TYPE_READ, ACCESS_SUSPICIOUS_IDENTITY_LOCK_STATUS,
+                    _internalUserService.getApiUser( author, clientCode ), null, SPECIFIC_ORIGIN );
+        }
+        catch( Exception e )
+        {
+            TransactionManager.rollBack( null );
+            response.setStatus(
+                    ResponseStatusFactory.failure( ).setMessage( e.getMessage( ) ).setMessageKey( Constants.PROPERTY_REST_ERROR_DURING_TREATMENT ) );
+        }
     }
 }
